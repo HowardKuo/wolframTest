3d8de91e545479f5007a6f86799e2cf4fe72c63e
HowardKuo <howardkuo91@gmail.com>
2019-07-27 19:55:49 -0700

    first commit

---
+++ wolfram.js
@@ -0,0 +1,25 @@
+var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+
+function doCORSRequest(options, printResult) {
+  var x = new XMLHttpRequest();
+  x.open(options.method, cors_api_url + options.url);
+
+  x.onload = x.onerror = function() {
+    responseObject = String(x.responseText)
+    // first = responseObject.indexOf("img") + 1
+    // firstSub = responseObject.substring(first)
+    // second = firstSub.indexOf("img") + 9
+    // secondSub = firstSub.substring(second)
+    // last = secondSub.indexOf("'")
+    // final = secondSub.substring(0,last).replace("&amp;","&")
+    printResult(JSON.parse(responseObject).queryresult);
+    // printResult(responseObject.queryresult)
+  };
+
+  x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  x.send(options.data);
+};
+
+function getNutritionalInfo(url, cb) {
+  doCORSRequest({ method: 'POST', url: url, data: url }, cb);
+}
